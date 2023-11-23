1b738cc8664e2a01e78a755aa3d023ba11198b9d
inzotik <innazotikova@gmail.com>
2023-11-23 17:48:52 -0600

    updated CookRunner

--- src/test/java/library/runner/CukesRunner.java
+++ src/test/java/library/runner/CukesRunner.java
@@ -9,13 +9,13 @@ import org.junit.runner.RunWith;
 
         plugin = {
                 "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt" ,
+                "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
-        features = "src/test/resources/features" ,
-        glue = "com/library/steps",
+        features = "src/test/resources/features",
+        glue = "library/steps",
         dryRun = false,
-        tags = "@us08"
+        tags = "@us01"
 )
 public class CukesRunner {
 
